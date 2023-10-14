16462ae4f235e90534bba1b0add524bd7d97e28c
SamIsrael <samisrael0407@gmail.com>
2023-10-14 19:10:36 +0530

    update -script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+// const prompt = require("prompt-sync")();
 console.log("----------Welcome to the ToDo project----------");
 
-let i=0;
-newList = []
-
-while (i==0) {
-    input = prompt("What would you like to do?");
+let breaki = 0;
+newList = [];
+while (breaki == 0) {
+    let input = prompt("What would you like to do? ");
 
     if (input == "new") {
-        newInput = prompt("Enter new todo");
+        newInput = prompt("Enter new todo ");
         newList.push(newInput);
     } else if (input == "list") {
-        console.log("*"*10);
-        for (let i =0; i<newList.length; i++) {
+        console.log("*".repeat(10));
+        if(newList.length <= 0) {
+            console.log("There are no items in the list");
+        }
+        for (let i = 0; i < newList.length; i++) {
             console.log(`${i}: ${newList[i]}`)
         }
-        console.log("*"*10);
+        console.log("*".repeat(10));
     } else if (input == "delete") {
-        deleteIndex = prompt ("Enter the index ");
-        newList = newList.splice(deleteIndex,1);
+        let deleteIndex = prompt("Enter the index ");
+        newList.splice(deleteIndex, 1);
     } else if (input == "quit") {
-        i+=1;
+        breaki += 1;
     }
 }
